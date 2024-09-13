21c2efce0ecc08e9e8f2bf5cc9eab475092e3827
JM-Bass <joaomariapereire@gmail.com>
2024-09-13 10:25:25 -0300

    FINAL BLOG

--- blog/views.py
+++ blog/views.py
@@ -9,3 +9,10 @@ def index(request):
     }
 
     return render(request, 'index.html', context)
+
+def postagem(request, id):
+    posts = Post.objects.get(pk=id)
+    context = {
+        "post": posts,
+    }
+    return render(request, 'post.html', context)
